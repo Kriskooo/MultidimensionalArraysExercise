1bbd9b36582ad519ee5d4d564d375618e71c4453
Kriskooo <kris.danielov@gmail.com>
2021-01-26 15:30:15 +0200

    MultidimensionalArraysExercise

---
+++ MultidimensionalArraysExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read matrix size
+            long matrixSize = long.Parse(Console.ReadLine());
+            //create matrix
+            long[,] matrix = new long[matrixSize, matrixSize];
+
+            //read the input - N(matrixsize) lines
+            for (long rows = 0; rows < matrixSize; rows++)
+            {
+                var input = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                //add array values to the matrix
+                for (long cols = 0; cols < matrixSize; cols++)
+                {
+                    matrix[rows, cols] = input[cols];
+                }
+                //Calculate diagonals
+            }
+            long leftDiagonal = 0;
+            long rightDiagonal = 0;
+
+            for (long cols = 0; cols < matrixSize; cols++)
+            {
+                leftDiagonal += matrix[cols, cols];
+                rightDiagonal += matrix[cols, matrixSize - cols - 1];
+                //         Console.WriteLine("[{0}, {1}] = {2}", cols, matrixSize-cols-1, matrix[cols][matrixSize - cols - 1]);
+            }
+
+            var result = Math.Abs(leftDiagonal - rightDiagonal);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ SquaresInMatrix/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            char[,] matrix = ReadMatrix(data[0], data[1]);
+            int count = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    bool isEqual = matrix[row, col] == matrix[row, col + 1] &&
+                        matrix[row + 1, col] == matrix[row + 1, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col];
+                    if (isEqual)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+        private static char[,] ReadMatrix(int rows, int cols)
+        {
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] rowDate = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowDate[col];
+                }
+            }
+            return matrix;
+        }
+
+    }
+}
